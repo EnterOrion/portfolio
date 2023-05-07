1730e795170ba11e8a0cc23a1709e7f0b8267cee
EnterOrion <orionwesson@gmail.com>
2023-05-08 03:26:45 +0530

    Add links to project section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,27 @@ const Footer: FC = () => {
   return (
     <footer>
       <div className="footer-links">
-        <i className="fa-solid fa-paper-plane" style={{ color: "#fafafa" }}></i>
-        <i className="devicon-github-original"></i>
-        <i className="devicon-linkedin-plain"></i>
+        <a href="mailto:orionwesson@gmail.com" target="_blank">
+          <i
+            className="fa-solid fa-paper-plane"
+            style={{ color: "#fafafa" }}
+          ></i>
+        </a>
+        <a href="https://github.com/EnterOrion" target="_blank">
+          <i className="devicon-github-original"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/orion-wesson-bb5b47148"
+          target="_blank"
+        >
+          <i className="devicon-linkedin-plain"></i>
+        </a>
       </div>
       <div className="attribution">
-        Made with ❤️ by <u>Orion Wesson</u>
+        <a href="https://github.com/EnterOrion" target="_blank">
+          {" "}
+          Made with ❤️ by <u>Orion Wesson</u>
+        </a>
       </div>
     </footer>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,8 +11,22 @@ const Projects: FC = () => {
         <div className="project-overview">
           <div className="project-name">Snail Pic of the Day</div>
           <div className="project-links">
-            <div className="github-link"></div>
-            <div className="preview-link"></div>
+            <div className="github-link">
+              <a
+                href="https://github.com/EnterOrion/snail-pic-of-the-day"
+                target="_blank"
+              >
+                Code
+              </a>
+            </div>
+            <div className="preview-link">
+              <a
+                href="https://enterorion.github.io/snail-pic-of-the-day/#/"
+                target="_blank"
+              >
+                Live
+              </a>
+            </div>
           </div>
           <div className="project-description">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi labore
@@ -26,8 +40,22 @@ const Projects: FC = () => {
         <div className="project-overview">
           <div className="project-name">Shopping Cart</div>
           <div className="project-links">
-            <div className="github-link"></div>
-            <div className="preview-link"></div>
+            <div className="github-link">
+              <a
+                href="https://github.com/EnterOrion/shopping-cart"
+                target="_blank"
+              >
+                Code
+              </a>
+            </div>
+            <div className="preview-link">
+              <a
+                href="https://enterorion.github.io/shopping-cart/#/"
+                target="_blank"
+              >
+                Live
+              </a>
+            </div>
           </div>
           <div className="project-description">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
@@ -43,8 +71,22 @@ const Projects: FC = () => {
         <div className="project-overview">
           <div className="project-name">Where is the cat?</div>
           <div className="project-links">
-            <div className="github-link"></div>
-            <div className="preview-link"></div>
+            <div className="github-link">
+              <a
+                href="https://github.com/EnterOrion/where-is-the-cat"
+                target="_blank"
+              >
+                Code
+              </a>
+            </div>
+            <div className="preview-link">
+              <a
+                href="https://enterorion.github.io/where-is-the-cat/"
+                target="_blank"
+              >
+                Live
+              </a>
+            </div>
           </div>
           <div className="project-description">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
@@ -58,8 +100,22 @@ const Projects: FC = () => {
         <div className="project-overview">
           <div className="project-name">Dream Journal</div>
           <div className="project-links">
-            <div className="github-link"></div>
-            <div className="preview-link"></div>
+            <div className="github-link">
+              <a
+                href="https://github.com/EnterOrion/dream-journal"
+                target="_blank"
+              >
+                Code
+              </a>
+            </div>
+            <div className="preview-link">
+              <a
+                href="https://enterorion.github.io/dream-journal/"
+                target="_blank"
+              >
+                Live
+              </a>
+            </div>
           </div>
           <div className="project-description">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
@@ -75,8 +131,22 @@ const Projects: FC = () => {
         <div className="project-overview">
           <div className="project-name">Weather Application</div>
           <div className="project-links">
-            <div className="github-link"></div>
-            <div className="preview-link"></div>
+            <div className="github-link">
+              <a
+                href="https://github.com/EnterOrion/weather-app"
+                target="_blank"
+              >
+                Code
+              </a>
+            </div>
+            <div className="preview-link">
+              <a
+                href="https://enterorion.github.io/weather-app/"
+                target="_blank"
+              >
+                Live
+              </a>
+            </div>
           </div>
           <div className="project-description">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
